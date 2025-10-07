dc7b7e1f0cd7dd69477f68f27cf0c3a762415459
wtburdon <billyburdon@outlook.com>
2025-10-06 21:48:02 -0700

    added hashtable

--- p1_starter/hash.cpp
+++ p1_starter/hash.cpp
@@ -4,7 +4,56 @@
 
 using namespace std;
 
-int hash_function(string text) {
+Node::Node(std::string& key, std::string& data) : key(key), data(data), next(nullptr) {}
+
+HashTable::HashTable(int size) : size(size) {
+    table = new Node*[size];
+    for (int i = 0; i < size; i++) {
+        table[i] = nullptr;
+    }
+}
+
+HashTable::~HashTable() {
+    for (int i = 0; i < size; i++) {
+        Node* current = table[i];
+        while (current != nullptr) {
+            Node* toDelete = current;
+            current = current->next;
+            delete toDelete;
+        }
+    }
+
+    delete[] table;
+}
+
+void HashTable::insert(std::string& key, std::string& data) {
+    int index = hash_function(key) % size;
+    Node* newNode = new Node(key, data);
+
+    newNode->next = table[index];
+    table[index] = newNode;
+}
+
+std::string HashTable::search(const std::string& key) {
+    int index = hash_function(key) % size;
+    Node* current = table[index];
+
+    while (current != nullptr) {
+        if (current->key == key) {
+            return current->data;
+        }
+        current = current->next;
+    }
+
+    return "";
+}
+
+int hash_function(std::string text) {
     // Implement your own hash function here
+
+    int x = text.length();
+
+    int a = x % 5;
+
     return 1;
 }
--- p1_starter/hash.h
+++ p1_starter/hash.h
@@ -5,4 +5,31 @@
 
 using namespace std;
 
+#ifndef HASH_H
+#define HASH_H
+
+struct Node {
+    std::string key;
+    std::string data;
+    Node* next;
+
+    Node(std::string& key, std::string& data);
+};
+
+class HashTable {
+    public:
+        HashTable(int size);
+        ~HashTable();
+
+        void insert(std::string& key, std::string& data);
+        std::string search(const std::string& key);
+
+    private:
+        Node** table;
+        int size;
+};
+
+
 int hash_function(string text);
+
+#endif
